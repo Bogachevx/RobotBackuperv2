df8541dc6321b1a7076fa2ce0711e6fa510d1dc5
Bogachev Dmitrii <bogachevx@gmail.com>
2018-06-27 22:31:21 +0300

    Initial commit

---
+++ RobotBackuperv2/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace RobotBackuperv2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.boxPortList = new System.Windows.Forms.ComboBox();
+            this.textConsole = new System.Windows.Forms.TextBox();
+            this.trigRS232 = new System.Windows.Forms.Button();
+            this.trigUSB = new System.Windows.Forms.Button();
+            this.textRobotName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // boxPortList
+            // 
+            this.boxPortList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.boxPortList.FormattingEnabled = true;
+            this.boxPortList.Location = new System.Drawing.Point(12, 12);
+            this.boxPortList.Name = "boxPortList";
+            this.boxPortList.Size = new System.Drawing.Size(121, 21);
+            this.boxPortList.TabIndex = 0;
+            this.boxPortList.SelectedIndexChanged += new System.EventHandler(this.boxPortList_SelectedIndexChanged);
+            // 
+            // textConsole
+            // 
+            this.textConsole.Location = new System.Drawing.Point(140, 12);
+            this.textConsole.Multiline = true;
+            this.textConsole.Name = "textConsole";
+            this.textConsole.Size = new System.Drawing.Size(496, 275);
+            this.textConsole.TabIndex = 1;
+            // 
+            // trigRS232
+            // 
+            this.trigRS232.Location = new System.Drawing.Point(26, 118);
+            this.trigRS232.Name = "trigRS232";
+            this.trigRS232.Size = new System.Drawing.Size(65, 40);
+            this.trigRS232.TabIndex = 2;
+            this.trigRS232.Text = "RS232";
+            this.trigRS232.UseVisualStyleBackColor = true;
+            // 
+            // trigUSB
+            // 
+            this.trigUSB.Location = new System.Drawing.Point(26, 164);
+            this.trigUSB.Name = "trigUSB";
+            this.trigUSB.Size = new System.Drawing.Size(65, 40);
+            this.trigUSB.TabIndex = 3;
+            this.trigUSB.Text = "USB";
+            this.trigUSB.UseVisualStyleBackColor = true;
+            this.trigUSB.Click += new System.EventHandler(this.trigUSB_Click);
+            // 
+            // textRobotName
+            // 
+            this.textRobotName.Location = new System.Drawing.Point(12, 62);
+            this.textRobotName.Name = "textRobotName";
+            this.textRobotName.Size = new System.Drawing.Size(121, 20);
+            this.textRobotName.TabIndex = 4;
+            this.textRobotName.Text = "Backup";
+            this.textRobotName.TextChanged += new System.EventHandler(this.textRobotName_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textRobotName);
+            this.Controls.Add(this.trigUSB);
+            this.Controls.Add(this.trigRS232);
+            this.Controls.Add(this.textConsole);
+            this.Controls.Add(this.boxPortList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox boxPortList;
+        private System.Windows.Forms.TextBox textConsole;
+        private System.Windows.Forms.Button trigRS232;
+        private System.Windows.Forms.Button trigUSB;
+        private System.Windows.Forms.TextBox textRobotName;
+    }
+}
+
---
+++ RobotBackuperv2/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RobotBackuperv2
+{
+    public partial class Form1 : Form
+    {
+        volatile SerialPort serialPort;
+        volatile bool isConnected = false;
+        volatile bool isBusy = false;
+        volatile int mode = 0;
+        volatile int portBoxSize;
+        volatile String portName;
+        volatile String robotName;
+        Thread tryConThread;
+        Thread checkConThread;
+        Thread downloadThread;
+        Thread portFinderThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+            findPorts();
+            tryConThread = new Thread(tryToConnect);
+            checkConThread = new Thread(checkConnection);
+            downloadThread = new Thread(downloadBackup);
+            //portFinderThread = new Thread(findPorts);
+            portName = " ";
+            checkConThread.Start();
+            consoleWrite("Trying to connect");
+            tryConThread.Start();
+            downloadThread.Start();
+            //portFinderThread.Start();
+        }
+
+        private void findPorts()
+        {
+            string[] Ports;
+            //while (true)
+            //{
+                Ports = SerialPort.GetPortNames();
+                portBoxSize = Ports.Length;
+
+                //if (boxPortList.Items.Count != portBoxSize)
+                //{
+                    boxPortList.Items.Clear();
+                    boxPortList.Items.AddRange(Ports);
+                    if (portBoxSize != 0)
+                    {
+                        boxPortList.SelectedIndex = 0;
+                    }
+                    consoleWrite("Found " + portBoxSize + " ports");
+                //}
+                
+                //Thread.Sleep(5000);
+            //}
+        }
+
+        private void asyncWrite(String text)
+        {
+
+            textConsole.Text += "[" + DateTime.Now.ToString("HH:mm:ss")
+                + "]: " + text + Environment.NewLine;
+            textConsole.SelectionStart = textConsole.Text.Length;
+            textConsole.ScrollToCaret();
+        }
+
+        private void consoleWrite(String text)
+        {
+            if (textConsole.InvokeRequired)
+                textConsole.Invoke(new Action(delegate () { asyncWrite(text); }));
+            else
+                asyncWrite(text);
+        }
+
+
+        void checkConnection()
+        {
+            while (true)
+            {
+                if (isConnected && !isBusy)
+                {
+                    
+                    try
+                    {
+                        serialPort.WriteLine("");
+                        string ad = serialPort.ReadTo(">");
+                       
+                        //break;
+
+                    }
+                    catch (Exception e)
+                    {
+                        isConnected = false;
+                        consoleWrite("Disconnected!");
+                        consoleWrite("Trying to connect!");
+                        serialPort.Close();
+                    }
+                }
+            }
+        }
+
+        void tryToConnect()
+        {
+            while (true)
+            {
+                if (isConnected == false)
+                {
+
+                        serialPort = new SerialPort(portName);
+                        serialPort.ReadTimeout = 1000;
+                        try
+                        {
+                            serialPort.Open();
+                            serialPort.WriteLine("");
+                            string ad = serialPort.ReadTo(">");
+                            consoleWrite("Connected to" + portName);
+                            isConnected = true;
+                        }
+                        catch (Exception e)
+                        {
+                            serialPort.Close();
+                        }
+                    
+                }
+            }
+        }
+
+        void downloadBackup()
+        {
+            while(true)
+            {
+                String dirName = robotName;
+                String resp;
+                robotName = DateTime.Now.ToString("ddMMyy");
+                switch (mode)
+                {
+                    case 1:
+                        serialPort.WriteLine("USB_FDEL " + dirName);
+                        while (true)
+                        {
+                            try
+                            {
+                                resp = serialPort.ReadTo(">");
+                                break;
+                            }
+                            catch (Exception ex) { };
+                        }
+                        //USB
+                        /*
+                         * resp = com.command("USB_FDEL " + dirName);              
+                comDelay();
+
+                resp = com.command("USB_MKDIR " + dirName);
+                comDelay();
+
+                if (resp[1].ToString().Substring(0, 2) == "(P")
+                {
+                    consoleWrite("Failed! Please input USB drive");
+                    return;
+                }
+                resp = com.command("USB_SAVE " + dirName + "\\" + RobotName);
+                comDelay();
+                consoleWrite("Backup saved");
+
+                resp = com.command("USB_SAVE/ELOG " + dirName + "\\" + RobotName);
+                comDelay();
+                consoleWrite("Error log saved");
+
+                resp = com.command("USB_SAVE/OPLOG " + dirName + "\\" + RobotName);
+                comDelay();
+                consoleWrite("Operation log saved");
+                         */
+                        consoleWrite("aaaaaaaa");
+                        mode = 0;
+                        //isBusy = false;
+                        break;
+                    case 2:
+                        //RS
+                        mode = 0;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void boxPortList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            portName = boxPortList.SelectedItem.ToString();
+            try
+            {
+                if (serialPort.IsOpen)
+                {
+                    serialPort.Close();
+                }
+                
+            } catch (Exception ex) { };
+            isConnected = false;
+        }
+
+        private void textRobotName_TextChanged(object sender, EventArgs e)
+        {
+            robotName = textRobotName.Text;
+        }
+
+        private void trigUSB_Click(object sender, EventArgs e)
+        {
+            isBusy = true;
+            mode = 1;
+        }
+    }
+}
